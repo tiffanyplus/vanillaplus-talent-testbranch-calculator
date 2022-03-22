080ca31be753eae198e4eb5b4b4b1b08571b1c63
tiffanyplus <100295960+tiffanyplus@users.noreply.github.com>
2022-03-22 16:22:33 +0100

    Update precache-manifest.0f12327b2382c0c08fc77c318730c259.js

--- precache-manifest.0f12327b2382c0c08fc77c318730c259.js
+++ precache-manifest.0f12327b2382c0c08fc77c318730c259.js
@@ -5,186 +5,186 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "016ad83e34513d54d5d9",
-    "url": "/vanillaplus-talent-calculator/static/css/0.3f93acc5.chunk.css"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/css/0.3f93acc5.chunk.css"
   },
   {
     "revision": "cc145698fe2da4652d1f",
-    "url": "/vanillaplus-talent-calculator/static/css/main.1ed946fa.chunk.css"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/css/main.1ed946fa.chunk.css"
   },
   {
     "revision": "016ad83e34513d54d5d9",
-    "url": "/vanillaplus-talent-calculator/static/js/0.0bbc3ade.chunk.js"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/js/0.0bbc3ade.chunk.js"
   },
   {
     "revision": "5877358b0c939a392223",
-    "url": "/vanillaplus-talent-calculator/static/js/10.acdc65ab.chunk.js"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/js/10.acdc65ab.chunk.js"
   },
   {
     "revision": "00327acb30d6bb8d02da",
-    "url": "/vanillaplus-talent-calculator/static/js/11.36123fee.chunk.js"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/js/11.36123fee.chunk.js"
   },
   {
     "revision": "3611a7d5a35c185dc18a",
-    "url": "/vanillaplus-talent-calculator/static/js/12.2dcca183.chunk.js"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/js/12.2dcca183.chunk.js"
   },
   {
     "revision": "2f33dc5ac6616a7770b8117950a39b8a",
-    "url": "/vanillaplus-talent-calculator/static/js/12.2dcca183.chunk.js.LICENSE"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/js/12.2dcca183.chunk.js.LICENSE"
   },
   {
     "revision": "bacd1322323d0a40dfe1",
-    "url": "/vanillaplus-talent-calculator/static/js/3.8ac4d176.chunk.js"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/js/3.8ac4d176.chunk.js"
   },
   {
     "revision": "2f706d80d350d219c176",
-    "url": "/vanillaplus-talent-calculator/static/js/4.79942d38.chunk.js"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/js/4.79942d38.chunk.js"
   },
   {
     "revision": "5e98b291677e03ad1af6",
-    "url": "/vanillaplus-talent-calculator/static/js/5.83886b51.chunk.js"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/js/5.83886b51.chunk.js"
   },
   {
     "revision": "4eb4c22d078b1961a999",
-    "url": "/vanillaplus-talent-calculator/static/js/6.5a8a05f5.chunk.js"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/js/6.5a8a05f5.chunk.js"
   },
   {
     "revision": "190b0599cb04f123cd33",
-    "url": "/vanillaplus-talent-calculator/static/js/7.69edcd1a.chunk.js"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/js/7.69edcd1a.chunk.js"
   },
   {
     "revision": "775a9e32222027cb5470",
-    "url": "/vanillaplus-talent-calculator/static/js/8.65e6464c.chunk.js"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/js/8.65e6464c.chunk.js"
   },
   {
     "revision": "5eae7167166199ea2875",
-    "url": "/vanillaplus-talent-calculator/static/js/9.755a9674.chunk.js"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/js/9.755a9674.chunk.js"
   },
   {
     "revision": "cc145698fe2da4652d1f",
-    "url": "/vanillaplus-talent-calculator/static/js/main.86a9a529.chunk.js"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/js/main.86a9a529.chunk.js"
   },
   {
     "revision": "b599f2a333984fa6efa3",
-    "url": "/vanillaplus-talent-calculator/static/js/runtime-main.351ed5f5.js"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/js/runtime-main.351ed5f5.js"
   },
   {
     "revision": "68b29a926e1bebd38e63343d7305e18b",
-    "url": "/vanillaplus-talent-calculator/static/media/affliction.68b29a92.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/affliction.68b29a92.jpg"
   },
   {
     "revision": "892c508ab326b2ad9c2e99fa8d449b8b",
-    "url": "/vanillaplus-talent-calculator/static/media/arcane.892c508a.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/arcane.892c508a.jpg"
   },
   {
     "revision": "3b1eb09954b17a81158141f6e8904d66",
-    "url": "/vanillaplus-talent-calculator/static/media/arms.3b1eb099.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/arms.3b1eb099.jpg"
   },
   {
     "revision": "2ba9eac3be306825332ee45dc3ea95ee",
-    "url": "/vanillaplus-talent-calculator/static/media/assassination.2ba9eac3.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/assassination.2ba9eac3.jpg"
   },
   {
     "revision": "e0beb1ab57f18f7e840de64bb3643658",
-    "url": "/vanillaplus-talent-calculator/static/media/balance.e0beb1ab.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/balance.e0beb1ab.jpg"
   },
   {
     "revision": "1e37fb0bdd7dcf042bbb1d90e7e5283e",
-    "url": "/vanillaplus-talent-calculator/static/media/beast-mastery.1e37fb0b.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/beast-mastery.1e37fb0b.jpg"
   },
   {
     "revision": "732693ea62975ccea098dcdc16e4c0b8",
-    "url": "/vanillaplus-talent-calculator/static/media/combat.732693ea.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/combat.732693ea.jpg"
   },
   {
     "revision": "fd92e13b4f078274ec04ed46d4fae453",
-    "url": "/vanillaplus-talent-calculator/static/media/demonology.fd92e13b.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/demonology.fd92e13b.jpg"
   },
   {
     "revision": "b0e11336eb5bf1ae15fbc446fb3cf1b4",
-    "url": "/vanillaplus-talent-calculator/static/media/destruction.b0e11336.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/destruction.b0e11336.jpg"
   },
   {
     "revision": "7ef542fd8d94cd6054e6a90f30d3716c",
-    "url": "/vanillaplus-talent-calculator/static/media/discipline.7ef542fd.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/discipline.7ef542fd.jpg"
   },
   {
     "revision": "ec420cae1396f99d4408709f24689863",
-    "url": "/vanillaplus-talent-calculator/static/media/elemental.ec420cae.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/elemental.ec420cae.jpg"
   },
   {
     "revision": "968052c44f47ad9d392a75dc037ce025",
-    "url": "/vanillaplus-talent-calculator/static/media/enhancement.968052c4.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/enhancement.968052c4.jpg"
   },
   {
     "revision": "b6a8478ee749da241e033f269585b888",
-    "url": "/vanillaplus-talent-calculator/static/media/feral.b6a8478e.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/feral.b6a8478e.jpg"
   },
   {
     "revision": "fbfd140b8e2c1824b3277400a9683f70",
-    "url": "/vanillaplus-talent-calculator/static/media/fire.fbfd140b.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/fire.fbfd140b.jpg"
   },
   {
     "revision": "514eba87a20832346a212ec52ae3c152",
-    "url": "/vanillaplus-talent-calculator/static/media/frost.514eba87.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/frost.514eba87.jpg"
   },
   {
     "revision": "5a9a21e2be8f6171ae0990d757115bec",
-    "url": "/vanillaplus-talent-calculator/static/media/fury.5a9a21e2.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/fury.5a9a21e2.jpg"
   },
   {
     "revision": "514967219f3500ea582a205697acbd4c",
-    "url": "/vanillaplus-talent-calculator/static/media/holy.51496721.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/holy.51496721.jpg"
   },
   {
     "revision": "b7f1381d53512475292ba07cdf38d28d",
-    "url": "/vanillaplus-talent-calculator/static/media/holy.b7f1381d.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/holy.b7f1381d.jpg"
   },
   {
     "revision": "0c9de8230cfedb8caaec5897d4a4ecb3",
-    "url": "/vanillaplus-talent-calculator/static/media/left--gold.0c9de823.png"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/left--gold.0c9de823.png"
   },
   {
     "revision": "e97d48addad74d2ba0814e7446146bbf",
-    "url": "/vanillaplus-talent-calculator/static/media/left.e97d48ad.png"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/left.e97d48ad.png"
   },
   {
     "revision": "905dc6290a10ff93ccf8cbe3ef7bae1f",
-    "url": "/vanillaplus-talent-calculator/static/media/marksmanship.905dc629.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/marksmanship.905dc629.jpg"
   },
   {
     "revision": "5a8a316871d45c2cd4e43619cae4ee47",
-    "url": "/vanillaplus-talent-calculator/static/media/protection.5a8a3168.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/protection.5a8a3168.jpg"
   },
   {
     "revision": "d8ff0256f76094710865faa62f8aa08f",
-    "url": "/vanillaplus-talent-calculator/static/media/protection.d8ff0256.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/protection.d8ff0256.jpg"
   },
   {
     "revision": "1a85983505591136cf6a34062d8f47f0",
-    "url": "/vanillaplus-talent-calculator/static/media/restoration.1a859835.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/restoration.1a859835.jpg"
   },
   {
     "revision": "e148424a4a100d7c14fe5b058cb0dd88",
-    "url": "/vanillaplus-talent-calculator/static/media/restoration.e148424a.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/restoration.e148424a.jpg"
   },
   {
     "revision": "17666e6ce0a9de255bd68bf20ea69469",
-    "url": "/vanillaplus-talent-calculator/static/media/retribution.17666e6c.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/retribution.17666e6c.jpg"
   },
   {
     "revision": "19c7b9ba4006a4f8727dcc1595260ec0",
-    "url": "/vanillaplus-talent-calculator/static/media/shadow.19c7b9ba.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/shadow.19c7b9ba.jpg"
   },
   {
     "revision": "3d6c94d0133968629b28b172d0dae3e0",
-    "url": "/vanillaplus-talent-calculator/static/media/subtlety.3d6c94d0.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/subtlety.3d6c94d0.jpg"
   },
   {
     "revision": "95ffdde50a6e9927d03347ef6a473dec",
-    "url": "/vanillaplus-talent-calculator/static/media/survival.95ffdde5.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/survival.95ffdde5.jpg"
   },
   {
     "revision": "e0f7232022e748a2b5b0e69b23d6c2cf",
-    "url": "/vanillaplus-talent-calculator/static/media/survival_instincts.e0f72320.jpg"
+    "url": "/vanillaplus-talent-testbranch-calculator/static/media/survival_instincts.e0f72320.jpg"
   }
 ]);
